c3bca7a23a170eddd23fc662305e16f7fb18e981
Bruno17 <b.perner@gmx.de>
2020-05-12 14:25:08 +0200

    second commit

---
+++ core/components/migxgpmhelper/model/migxgpmhelper/mysql/mghcategory.class.php
@@ -0,0 +1,3 @@
+<?php
+require_once (dirname(__DIR__) . '/mghcategory.class.php');
+class mghCategory_mysql extends mghCategory {}
---
+++ core/components/migxgpmhelper/model/migxgpmhelper/mysql/mghpackage.class.php
@@ -0,0 +1,3 @@
+<?php
+require_once (dirname(__DIR__) . '/mghpackage.class.php');
+class mghPackage_mysql extends mghPackage {}
